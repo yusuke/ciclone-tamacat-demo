d18a9a0ad02e1fd9713345362b4d15e04fb793e3
Yusuke Yamamoto <yusuke@mac.com>
2026-01-09 11:58:03 +0900

    改行を入れただけ

--- src/main/java/com/example/validationtest/ValidationTestApplication.java
+++ src/main/java/com/example/validationtest/ValidationTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ValidationTestApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(ValidationTestApplication.class, args);
     }
